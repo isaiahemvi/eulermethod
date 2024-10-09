79775aa583fbe45f8df3f91773f0363dba7efc3f
isaiahemvi <isaiahevirgil@gmail.com>
2024-10-09 09:10:50 -0400

    Cleaning/Updating documentation

--- eulersmethod.py
+++ eulersmethod.py
@@ -1,18 +1,9 @@
 from sympy import sympify, symbols
 import sympy
 
-
-# EULERS METHOD
-# Uses a compact, efficient recursive function to obtain ~F(N) through Euler's method 
-
-
-
 x,y = symbols('x y')
-equation = input("Enter dy/dx: ")
-expression = sympy.parsing.sympy_parser.parse_expr(equation)
-# result = expression.subs(x, 4)
-# result = result.subs(y, 2)
-# print(result)
+equation = input("Enter dy/dx: ") # gets dy/dx as user input 
+expression = sympy.parsing.sympy_parser.parse_expr(equation) # parses through user input and converts to a mathematical expression
 
 def EulersRecursive(xi,yi,h,n):
     yn = yi
@@ -28,17 +19,14 @@ def EulersRecursive(xi,yi,h,n):
         EulersRecursive(xn,yn,h,n)
  
 def EulersIterative(xi,yi,h,n):
-
     for a in range(0,n-xi):
-        
         xn = xi+a
-
         xsub = expression.subs(x, xn) 
         slope = xsub.subs(y, yi)
-
         yi = yi + (slope*h)
     print(yi)
 
+# INPUTS 
 xinput = int(input("What is the initial value of x?"))
 yinput = float(input("What is the initial value of y?"))
 hinput = float(input("What is your step size?"))
